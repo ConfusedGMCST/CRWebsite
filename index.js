a913b514e2d2fce749ca0e0c7d779863e651ab99
ConfusedG <christian.haase@mcvts.org>
2024-09-27 10:41:52 -0400

    stole even more code from 3xion
    yes

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ window.onload = function() {
 function homePage() {
     window.open("index.html","_self")
 }
+function companiesPage() {
+    window.open("companies.html","_self")
+}
+function infoPage() {
+    window.open("info.html","_self")
+}
