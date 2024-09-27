43ef4bd7b68492f87d247da277c8d858401e773f
ConfusedG <christian.haase@mcvts.org>
2024-09-27 13:31:35 -0400

    added some hover things
    yes

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-window.onload = function() {
-    const HomeButton = document.getElementById('homeButton');
-    HomeButton.addEventListener("mouseover", function() {
-        this.style.cursor = "pointer";
-    })
-    HomeButton.addEventListener("mouseout", function() {
-        this.style.cursor = "default";
-    })
-    HomeButton.addEventListener("mousedown", function() {
-        this.style.cursor = "default"
-        /*window.location.href = ""; I'll set when I deploy the website ¯\_(ツ)_/¯*/
-    })
-}
-
 function homePage() {
     window.open("index.html","_self")
 }
