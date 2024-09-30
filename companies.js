9962665a02c0c2c47a9d47c99fb22b6bd439117c
ConfusedG <christian.haase@mcvts.org>
2024-09-30 09:19:32 -0400

    did a lot of stuff
    for the companies page

---
+++ companies.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    const compHeading = document.getElementById("companiesHeading");
+    const apple = document.getElementById("apple");
+    const ferrari = document.getElementById("ferrari");
+    compHeading.style.opacity = "0.5";
+    compHeading.style.fontSize = "300%";
+    compHeading.style.textShadow = "10px 2px black";
+    apple.style.marginLeft = "20%";
+    ferrari.style.marginLeft = "20%";
+    //compHeading.style.color = "#fb2500";
+}
