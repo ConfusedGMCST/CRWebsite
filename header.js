3143f6fffda891147911ae536ed44768a71298a2
ConfusedG <christian.haase@mcvts.org>
2024-10-02 09:16:38 -0400

    added a new theme mode
    got rid of the gradient
    need to make it consistent when you change pages
    I'll figure it out later.

--- header.js
+++ header.js
@@ -1,3 +1,5 @@
+let darkTheme = false
+
 function homePage() {
     window.open("index.html","_self")
 }
@@ -6,4 +8,26 @@ function companiesPage() {
 }
 function infoPage() {
     window.open("info.html","_self")
+}
+function themeButton() {
+    const consumerHeader = document.getElementById("consumerHeader");
+    const homeButton = document.getElementById("homeButton");
+    const infoButton = document.getElementById("infoButton");
+    const companiesButton = document.getElementById("companiesButton");
+    const body = document.body;
+    if (darkTheme == true) {
+        darkTheme = false;
+        body.style.backgroundImage = "linear-gradient(90deg, #a6a6a6, #cdcdcd)";
+        consumerHeader.style.color = "black";
+        homeButton.style.color = "black";
+        infoButton.style.color = "black";
+        companiesButton.style.color = "black";
+    } else {
+        darkTheme = true;
+        body.style.backgroundImage = "linear-gradient(90deg, #3d3d3d, #4e4e4e)";
+        consumerHeader.style.color = "white";
+        homeButton.style.color = "white";
+        infoButton.style.color = "white";
+        companiesButton.style.color = "white";
+    }
 }
