578849ba79ca30dd1334c570d406b8125880cdd1
ConfusedG <christian.haase@mcvts.org>
2024-10-10 08:35:09 -0400

    fresh start
    haven't worked on the proj in too long and I don't feel like reworking the code so I'm just restarting it

--- companies.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    const compHeading = document.getElementById("companiesHeading");
-    const apple = document.getElementById("apple");
-    const ferrari = document.getElementById("ferrari");
-    compHeading.style.opacity = "0.5";
-    compHeading.style.fontSize = "300%";
-    compHeading.style.textShadow = "10px 2px black";
-    //compHeading.style.color = "#fb2500";
-}
